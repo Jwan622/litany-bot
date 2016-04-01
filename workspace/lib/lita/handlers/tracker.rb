f71d95307a682be121479fa9ac13726606fc6d73
Jeffrey Wan <jeffrey.wan@blueapron.com>
2016-04-01 15:58:50 -0400

    deploy time

--- workspace/lib/lita/handlers/tracker.rb
+++ workspace/lib/lita/handlers/tracker.rb
@@ -8,8 +8,23 @@ module Lita
         '/\Ahttps?:\/\/open\.spotify\.com\/track\/(\w+)/i' => 'uploads your track and related info up to the BlueApron music site'
       })
 
-      route(/hi/) do |response|
+      http.get "/tracks" do |request, response|
+        track_array = redis.smembers("tracks").map do |track|
+          track
+        end
+        response.body << { tracks: track_array }.to_json
+      end
 
+      http.get "/tracks/:user" do |request, response|
+        response.body = redis.smembers(:user)
+      end
+
+      http.get "/tracks/:genre" do |request, response|
+        response.body << redis.smembers(:genre)
+      end
+
+      http.get "/tracks/:id" do |request, response|
+        response.body = redis.hgetall(:id)
       end
 
       def find_and_save_track(response)
@@ -18,15 +33,18 @@ module Lita
         unless track
           message = "Sorry, I couldn't find the Spotify track that you asked for."
         else
-          redis_track_key = response.user.name + track.id
-          # individual track key stored
-          db_connection.mapped_hmset(redis_track_key, single_track_json(track))
+          # store all tracks
+          redis.sadd("tracks", single_track_json(track, response.user.name))
 
-          # store for all users using set add
-          db_connection.sadd(response.user, redis_track_key)
+          # store individual track
+          redis.mapped_hmset(track.id, single_track_json(track, response.user.name))
 
-          # store for genre this doesnt' work because apparently last.fm relies on user input which may not be helpful
-          db_connection.sadd(track.album.genres.first, redis_track_key)
+          # store individual track for user, set add
+          redis_user_key = response.user.name + track.id
+          redis.sadd(redis_user_key, track.id)
+
+          # store individual track for genre. this doesnt' work because apparently last.fm relies on user input which may not be helpful
+          redis.sadd(track.album.genres.first, track.id)
 
           message = "We saved and uploaded your track: #{track.name}"
         end
@@ -36,29 +54,30 @@ module Lita
 
       private
 
-      def db_connection
-        $redis ||= Redis.new
-      end
-
       def obtain_spotify_track(response)
         user_input = response.message.body
 
         Lita::Handlers::Spotifier.find(user_input)
       end
 
-      def single_track_json(track)
+      def single_track_json(track, user)
         track_name = track.name
+        artist = track.artists.first.name
         preview_url = track.preview_url
-        track_album = track.album
+        track_release_date = track.album.release_date
         track_album_name = track.album.name
         track_duration_time = Time.at(track.duration_ms / 1000).utc.strftime("%M:%S")
+        images = track.album.images
 
         {
+          user: user,
           track_name: track_name,
+          artist: artist,
           preview_url: preview_url,
-          track_album: track_album,
+          track_release_date: track_release_date,
           track_album_name: track_album_name,
-          track_duration_time: track_duration_time
+          track_duration_time: track_duration_time,
+          images: images
         }
 
       end
